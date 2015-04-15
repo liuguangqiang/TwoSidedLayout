38b96bfc23eabb85d0f36f22cf819dfcc6a53c56
Eric <guangqiang.dev@gmail.com>
2015-04-15 18:00:31 +0800

    fixed.

--- app/src/main/java/com/liuguangqiang/turnlayout/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/turnlayout/sample/MainActivity.java
@@ -1,40 +1,28 @@
 package com.liuguangqiang.turnlayout.sample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
 
-import com.liuguangqiang.turnlayout.R;
+import com.liuguangqiang.turnlayout.widget.TurnLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TurnLayout turnLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initViews() {
+        turnLayout = (TurnLayout) findViewById(R.id.turnlayout);
+        turnLayout.setOrientation(TurnLayout.HORIZONTAL);
+        turnLayout.setTensionAndFriction(30, 15);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
