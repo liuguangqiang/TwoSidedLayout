40d3256f565c020cb4a6ed20743b93eb00cfe0d8
Eric <guangqiang.liu@symbio.com>
2018-04-25 15:32:23 +0800

    Update gradle config.

--- app/src/main/java/com/liuguangqiang/turnlayout/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/turnlayout/sample/MainActivity.java
@@ -1,27 +1,25 @@
 package com.liuguangqiang.turnlayout.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import com.liuguangqiang.turnlayout.widget.TurnLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private TurnLayout turnLayout;
+  private TurnLayout turnLayout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initViews();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    initViews();
+  }
 
-    private void initViews() {
-        turnLayout = (TurnLayout) findViewById(R.id.turnlayout);
-        turnLayout.setOrientation(TurnLayout.HORIZONTAL);
-        turnLayout.setTensionAndFriction(30, 15);
-        turnLayout.setTurnable(true);
-    }
-
+  private void initViews() {
+    turnLayout = findViewById(R.id.turnlayout);
+    turnLayout.setOrientation(TurnLayout.HORIZONTAL);
+    turnLayout.setTensionAndFriction(30, 15);
+    turnLayout.setTurnable(true);
+  }
 }
